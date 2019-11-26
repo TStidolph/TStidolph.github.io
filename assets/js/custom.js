89db46ea42b971f479b414d399c2b55ad10f2fa3
TStidolph <58085193+TStidolph@users.noreply.github.com>
2019-11-26 02:51:54 +0000

    Added functional clock.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,16 @@
+function initialiseClock(){
+  var currentDate = new Date();
+  var hour = currentDate.getHours();
+  var minute = currentDate.getMinutes();
+  var second = currentDate.getSeconds();
+    
+  document.getElementById('clock').innerHTML = "Current time (GMT): "+hour+":"+minute+":"+second;
+    
+  var timer = setTimeout(initialiseClock,1000);
+};
+
+window.onload = initialiseClock();
+
 var closeText = document.getElementById("text-hide");
 var initialText = document.getElementById("initial-text");
 
