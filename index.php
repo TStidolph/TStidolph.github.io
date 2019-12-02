f3863f3b9a5fe3d512bf34f176f955b9bd561dd0
TStidolph <58085193+TStidolph@users.noreply.github.com>
2019-12-02 15:54:10 +0000

    Added batch file to make exporting better.

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/rtop.videoPlayer.1.0.2.min.css">
+    <link rel="stylesheet" href="assets/css/smoothproducts.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+        <div class="container"><a class="navbar-brand logo" href="index.html">Muse Fanpage</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse"
+                id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="index.html">Home</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="requirements.html">Requirements</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="login.html">LOGIN</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="registration.html">Register</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="page landing-page">
+        <section id="initial-text" class="clean-block clean-hero" style="background-image: url(&quot;assets/img/Muse-Logo.png&quot;);color: rgba(0,0,0,0.64);origin: /* http://wallpaperlepi.com/wp-content/uploads/2014/08/Muse-Black-Image-Wallpaper-HD.jpg */;">
+            <div class="text">
+                <h1 class="text-uppercase">Muse</h1>
+                <p>Welcome to Thomas's Muse Fanpage! Learn about the band, their music, and choose your favorites!</p><button class="btn btn-outline-light btn-lg" id="text-hide" type="button">Close</button></div>
+        </section>
+        <div class="row">
+            <div class="col">
+                <div id="uprising-video" style="display: none;width: 80%;margin: auto;padding: 100px;" data-type="video/mp4" data-poster="#" data-video="#"></div>
+            </div>
+        </div>
+        <section class="clean-block clean-info dark" style="padding-bottom: 0px;">
+            <div class="container">
+                <div class="block-heading">
+                    <h2 class="text-info">Albums</h2>
+                    <p>The newest albums released by Muse.</p>
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-md-6"><img class="img-thumbnail" src="assets/img/simulation-theory.png" alt="Simulation Theory album cover" origin="https://pieandvinylonline.co.uk/wp-content/uploads/2018/09/Muse-Cover.jpg"></div>
+                    <div class="col-md-6">
+                        <h3>Simulation Theory</h3>
+                        <div class="getting-started-info">
+                            <p>Released in 2018, this is Muse's newest album.</p>
+                        </div><button class="btn btn-outline-primary btn-lg" id="simulation-button" type="button">Toggle Songs</button></div>
+                </div>
+                <div class="row align-items-center" id="simulation-theory" style="display: none;">
+                    <div class="col">
+                        <ul>
+                            <li>Algorithm</li>
+                            <li>The Dark Side</li>
+                            <li>Pressure</li>
+                            <li>Propaganda</li>
+                            <li>Break it to Me</li>
+                            <li>Something Human</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <ul>
+                            <li>Thought Contagion</li>
+                            <li>Get Up and Fight</li>
+                            <li>Blockades</li>
+                            <li>Dig Down</li>
+                            <li>The Void</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-md-6"><img class="img-thumbnail" src="assets/img/drones.png" alt="Drones album cover" origin="https://crownnote.com/sites/default/files/drones.jpg"></div>
+                    <div class="col-md-6">
+                        <h3>Drones</h3>
+                        <div class="getting-started-info">
+                            <p>This album came out in 2015 as Muse's 7th release.</p>
+                        </div><button class="btn btn-outline-primary btn-lg" id="drones-button" type="button">Toggle Songs</button></div>
+                </div>
+                <div class="row align-items-center" id="drones" style="display: none;">
+                    <div class="col">
+                        <ul>
+                            <li>Dead Inside</li>
+                            <li>The Dark Side</li>
+                            <li>Psycho</li>
+                            <li>Mercy</li>
+                            <li>Reapers</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <ul>
+                            <li>The Handler</li>
+                            <li>Defector</li>
+                            <li>Revolt</li>
+                            <li>Aftermath</li>
+                            <li>The Globalist</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="clean-block about-us" style="background-color: rgb(246,246,246);">
+            <div class="container">
+                <div class="block-heading" style="padding-top: 40px;">
+                    <h2 class="text-info">Band Members</h2>
+                    <p>The 3 members of Muse have been there from the start, and continue to produce and perform.</p>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="card clean-card text-center"><img class="card-img-top w-100 d-block" src="assets/img/matthew-bellamy.png" alt="Matthew Bellamy portrait" origin="https://www.thefamouspeople.com/profiles/thumbs/matthew-bellamy-1.jpg">
+                            <div class="card-body info">
+                                <h4 class="card-title">Matthew Bellamy</h4>
+                                <p class="card-text">Commonly known as Matt, the lead singer and guitarist of the band.</p>
+                                <div class="icons"><a href="https://www.facebook.com/MatthewJamesBellamy"><i class="icon-social-facebook"></i></a><a href="https://www.instagram.com/mattbellamy/"><i class="icon-social-instagram"></i></a><a href="https://twitter.com/MattBellamy"><i class="icon-social-twitter"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="card clean-card text-center"><img class="card-img-top w-100 d-block" src="assets/img/christopher-wolstenholme.png" alt="Christopher Wolstenholme Portrait" origin="https://www.famousbirthdays.com/faces/wolstenholme-christopher-image.jpg">
+                            <div class="card-body info">
+                                <h4 class="card-title">Christopher Wolstenholme</h4>
+                                <p class="card-text">Christopher plays bass, guitar and keyboard. He also performs backing lyrics.</p>
+                                <div class="icons"><a href="https://www.facebook.com/TheWolstenbeast"><i class="icon-social-facebook"></i></a><a href="https://www.instagram.com/ctwolstenholme78/"><i class="icon-social-instagram"></i></a><a href="https://twitter.com/ctwolstenholme"><i class="icon-social-twitter"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="card clean-card text-center"><img class="card-img-top w-100 d-block" src="assets/img/dominic-howard.png" alt="Dominic Howard portrait" origin="https://www.famousbirthdays.com/headshots/dominic-howard-1.jpg">
+                            <div class="card-body info">
+                                <h4 class="card-title">Dominic Howard</h4>
+                                <p class="card-text">The band's drummer/percussion player.</p>
+                                <div class="icons"><a href="https://www.facebook.com/pages/Dominic-Howard/104108579626621"><i class="icon-social-facebook"></i></a><a href="https://www.instagram.com/domhoward77/"><i class="icon-social-instagram"></i></a><a href="https://twitter.com/Dominic_Howard"><i class="icon-social-twitter"></i></a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <div class="row">
+                <div class="col text-center align-self-baseline">
+                    <p id="clock" style="margin-bottom: 0.1rem;font-size: 0.8rem;">Current time (GMT):&nbsp;</p>
+                </div>
+            </div>
+        </footer>
+    </main>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="assets/js/smoothproducts.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+    <script src="assets/js/rtop.videoPlayer.1.0.2.min.js"></script>
+    <script src="assets/js/custom.js"></script>
+</body>
+
+</html>
